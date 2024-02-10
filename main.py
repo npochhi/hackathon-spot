30ce88623ec3dfb72dfb7c1c194f4b6581f443e4
Nilay Pochhi <npochhi@rippling.com>
2024-02-10 15:46:00 -0800

    u

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import time
 from spot_controller import SpotController
+import cv2
 
 ROBOT_IP = "192.168.6.9"#os.environ['ROBOT_IP']
 SPOT_USERNAME = "admin"#os.environ['SPOT_USERNAME']
@@ -8,20 +9,57 @@ SPOT_PASSWORD = "2zqa8dgw7lor"#os.environ['SPOT_PASSWORD']
 
 
 def main():
-
     # Use wrapper in context manager to lease control, turn on E-Stop, power on robot and stand up at start
     # and to return lease + sit down at the end
     with SpotController(username=SPOT_USERNAME, password=SPOT_PASSWORD, robot_ip=ROBOT_IP) as spot:
-
         time.sleep(2)
+    
+        camera_capture = cv2.VideoCapture(0)
+
+        _, center_image = camera_capture.read()
+        camera_capture.release()
 
         # Move head to specified positions with intermediate time.sleep
         spot.move_head_in_points(yaws=[0.2, 0],
-                                 pitches=[0.3, 0],
-                                 rolls=[0.4, 0],
+                                 pitches=[0, 0],
+                                 rolls=[0, 0],
                                  sleep_after_point_reached=1)
         time.sleep(3)
 
+        _, left_image = camera_capture.read()
+        camera_capture.release()
+
+        spot.move_head_in_points(yaws=[0.8, 0],
+                                 pitches=[0, 0],
+                                 rolls=[0, 0],
+                                 sleep_after_point_reached=1)
+        
+        time.sleep(3)
+
+        _, right_image = camera_capture.read()
+        camera_capture.release()
+
+        spot.move_head_in_points(yaws=[0, 0],
+                                 pitches=[0.2, 0],
+                                 rolls=[0, 0],
+                                 sleep_after_point_reached=1)
+        
+        time.sleep(3)
+
+        _, top_image = camera_capture.read()
+        camera_capture.release()
+
+        spot.move_head_in_points(yaws=[0, 0],
+                                 pitches=[0.8, 0],
+                                 rolls=[0, 0],
+                                 sleep_after_point_reached=1)
+
+        time.sleep(3)
+
+        _, bottom_image = camera_capture.read()
+        camera_capture.release()
+
+        
         # Make Spot to move by goal_x meters forward and goal_y meters left
         spot.move_to_goal(goal_x=0.5, goal_y=0)
         time.sleep(3)
