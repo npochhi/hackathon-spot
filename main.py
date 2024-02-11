48b35b3f68fdbe8a1fbf4bc38606225bcc96e1bb
Nilay Pochhi <npochhi@rippling.com>
2024-02-10 16:51:01 -0800

    u

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import time
 from spot_controller import SpotController
+import cv2
 
 ROBOT_IP = "192.168.50.3"#os.environ['ROBOT_IP']
 SPOT_USERNAME = "admin"#os.environ['SPOT_USERNAME']
@@ -8,35 +9,57 @@ SPOT_PASSWORD = "2zqa8dgw7lor"#os.environ['SPOT_PASSWORD']
 
 
 def main():
-    #example of using micro and speakers
-    print("Start recording audio")
-    sample_name = "aaaa.wav"
-    cmd = f'arecord -vv --format=cd --device={os.environ["AUDIO_INPUT_DEVICE"]} -r 48000 --duration=10 -c 1 {sample_name}'
-    print(cmd)
-    os.system(cmd)
-    print("Playing sound")
-    os.system(f"ffplay -nodisp -autoexit -loglevel quiet {sample_name}")
-    
-    # Capture image
-    import cv2
-    camera_capture = cv2.VideoCapture(0)
-    rv, image = camera_capture.read()
-    print(f"Image Dimensions: {image.shape}")
-    camera_capture.release()
-
-    # Use wrapper in context manager to lease control, turn on E-Stop, power on the robot and stand up at start
+    # Use wrapper in context manager to lease control, turn on E-Stop, power on robot and stand up at start
     # and to return lease + sit down at the end
     with SpotController(username=SPOT_USERNAME, password=SPOT_PASSWORD, robot_ip=ROBOT_IP) as spot:
-
         time.sleep(2)
+    
+        camera_capture = cv2.VideoCapture(0)
+
+        _, center_image = camera_capture.read()
+        camera_capture.release()
 
         # Move head to specified positions with intermediate time.sleep
         spot.move_head_in_points(yaws=[0.2, 0],
-                                 pitches=[0.3, 0],
-                                 rolls=[0.4, 0],
+                                 pitches=[0, 0],
+                                 rolls=[0, 0],
                                  sleep_after_point_reached=1)
         time.sleep(3)
 
+        _, left_image = camera_capture.read()
+        camera_capture.release()
+
+        spot.move_head_in_points(yaws=[0.8, 0],
+                                 pitches=[0, 0],
+                                 rolls=[0, 0],
+                                 sleep_after_point_reached=1)
+        
+        time.sleep(3)
+
+        _, right_image = camera_capture.read()
+        camera_capture.release()
+
+        spot.move_head_in_points(yaws=[0, 0],
+                                 pitches=[0.2, 0],
+                                 rolls=[0, 0],
+                                 sleep_after_point_reached=1)
+        
+        time.sleep(3)
+
+        _, top_image = camera_capture.read()
+        camera_capture.release()
+
+        spot.move_head_in_points(yaws=[0, 0],
+                                 pitches=[0.8, 0],
+                                 rolls=[0, 0],
+                                 sleep_after_point_reached=1)
+
+        time.sleep(3)
+
+        _, bottom_image = camera_capture.read()
+        camera_capture.release()
+
+        
         # Make Spot to move by goal_x meters forward and goal_y meters left
         spot.move_to_goal(goal_x=0.5, goal_y=0)
         time.sleep(3)
